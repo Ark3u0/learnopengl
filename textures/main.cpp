0cd0efa2c0e98f4f0095bbc9a8cc4c15d333b268
sgarcia <sjgarcia@uchicago.edu>
2016-09-10 14:42:15 -0400

    Initialize textures lesson repo and link SOIL library

---
+++ textures/main.cpp
@@ -0,0 +1,119 @@
+// GLEW
+#include <GL/glew.h>
+#include <SOIL.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <cmath>
+
+#include "shader.h"
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <SOIL.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <cmath>
+
+#include "shader.h"
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, keyCallback);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    int framebufferWidth, framebufferHeight;
+    glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
+    glViewport(0, 0, framebufferWidth, framebufferHeight);
+
+    Shader ourShader("/Users/sgarcia/Code/C++/opengl/shaders/shader.vs", "/Users/sgarcia/Code/C++/opengl/shaders/shader.frag");
+
+    GLuint vertexArrayObject, vertexBufferObject;
+
+    // Initial triangle
+    GLfloat triangleVertices[] = {
+            // Positions        // Colors
+             0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f, // Bottom Right
+            -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // Bottom Left
+             0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f // Top
+    };
+
+    glGenVertexArrays(1, &vertexArrayObject);
+    glGenBuffers(1, &vertexBufferObject);
+
+    // Bind Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+    glBindVertexArray(vertexArrayObject);
+
+    // Copy triangleVertices array in a buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
+
+    // Position Attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    // Color Attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Unbind the Vertex Array Object;
+    glBindVertexArray(0);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ourShader.Use();
+        glBindVertexArray(vertexArrayObject);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        glfwSwapBuffers(window);
+    }
+    glDeleteVertexArrays(1, &vertexArrayObject);
+    glDeleteBuffers(1, &vertexBufferObject);
+    glfwTerminate();
+    return 0;
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
